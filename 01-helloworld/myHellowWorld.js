1dc7a926d27a6b1880e6e796be6fe55aafa7e31e
Cornell Reddick <cornellreddick@outlook.com>
2022-08-12 06:37:02 -0400

    Full Stack Practice

---
+++ 01-helloworld/myHellowWorld.js
@@ -0,0 +1,4 @@
+var firtName = "Cornell";
+var lastName = "Reddick";
+console.log(firtName + " " + lastName);
+console.log("Hi ".concat(firtName, " ").concat(lastName));
---
+++ 01-helloworld/myHellowWorld.ts
@@ -0,0 +1,6 @@
+let firtName: string = "Cornell";
+let lastName: string = "Reddick";
+
+
+console.log(firtName + " " + lastName);
+console.log(`Hi ${firtName} ${lastName}`)
