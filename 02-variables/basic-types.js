c013693101741ea0420d60f76cfa66dcf8b832de
Cornell Reddick <cornellreddick@outlook.com>
2022-08-12 06:46:05 -0400

    Practicing TypeScript variables

---
+++ 02-variables/basic-types.js
@@ -0,0 +1,7 @@
+var source = true;
+var grade = 99.9;
+var fName = "Cornell";
+var lName = "Reddick";
+console.log(source);
+console.log(grade);
+console.log(fName + " " + lName);
---
+++ 02-variables/basic-types.ts
@@ -0,0 +1,10 @@
+let source: boolean = true;
+let grade: number = 99.9;
+let fName: string = "Cornell";
+let lName: string = "Reddick";
+
+console.log(source);
+console.log(grade);
+
+console.log(fName + " " + lName);
+
